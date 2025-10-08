9bccc7ed70cbd3ab7da0ec90e6012d28cc485a08
sabrina <sabrina130102@gmail.com>
2025-10-08 15:38:46 +0200

    Edited css in  work, about and  skills folder to fix margin alignment

--- src/pages/Work/Work.jsx
+++ src/pages/Work/Work.jsx
@@ -36,7 +36,7 @@ export const MyWork = () => {
 
     return (
         <section id="work" className="projects">
-            <div className="project-container">
+            <div>
                 <h1 className="section-title">My latest work</h1>
 
                 <div className="project-grid">
@@ -51,41 +51,44 @@ export const MyWork = () => {
                         </div>
                         
                         {/* Project Header */}
-                        <div className="column-project">
-                            <h2 className="project-title">{project.title}</h2>
-                            <p className="type">{project.type} Project</p>
-                            <p>
-                                Role: <span className="role">{project.role}</span>
-                            </p>
-                        </div>
-
-                        {/* Project Description */}
-                        <div className="project-content">
-                            <div className="project-tags">
-                                {project.tags.map((tag, i) => (
-                                <span key={i} className="tag">
-                                    {tag}
-                                </span>
-                                ))}
+                        <div className="project-container">
+                                <div className="column-project">
+                                <h2 className="project-title">{project.title}</h2>
+                                <p className="type">{project.type} Project</p>
+                                <p>
+                                    Role: <span className="role">{project.role}</span>
+                                </p>
                             </div>
 
-                            <p className="desc">{project.description}</p>
-                            
-                            <div className="desc-link">
-                                {renderLink(project)}
+                            {/* Project Description */}
+                            <div className="project-content">
+                                <div className="project-tags">
+                                    {project.tags.map((tag, i) => (
+                                    <span key={i} className="tag">
+                                        {tag}
+                                    </span>
+                                    ))}
+                                </div>
 
-                                {project.demo && (
+                                <p className="desc">{project.description}</p>
                                 
-                                    <a href={project.demo} 
-                                    className="demo" 
-                                    target="_blank" 
-                                    rel="noopener noreferrer"
-                                    >
-                                        Live Demo <span className="arrow"></span>
-                                    </a>
-                                )}
+                                <div className="desc-link">
+                                    {renderLink(project)}
+
+                                    {project.demo && (
+                                    
+                                        <a href={project.demo} 
+                                        className="demo" 
+                                        target="_blank" 
+                                        rel="noopener noreferrer"
+                                        >
+                                            Live Demo <span className="arrow"></span>
+                                        </a>
+                                    )}
+                                </div>
                             </div>
                         </div>
+                        
                     </div>
                     ))}
                 </div>
