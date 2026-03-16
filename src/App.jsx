fac4bcc6412f73f9c22e8b4b0edc46e409d5eb2e
sabrina <sabrina130102@gmail.com>
2026-03-16 21:12:57 +0100

    fix folder case

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import ProjectPage from "./Pages/SideProject/ProjectPage.jsx";
-import HomePage from "./Pages/Home/HomeSection.jsx";
+import ProjectPage from "./page/SideProject/ProjectPage.jsx";
+import HomePage from "./page/Home/HomeSection.jsx";
 import Navbar from "./component/Navbar/Navbar.jsx";
 import Footer from "./component/Footer/Footer.jsx";
 
