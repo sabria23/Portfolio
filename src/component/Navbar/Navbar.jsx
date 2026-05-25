c30d8094f7101e6640f203bc110fd86e4a7ea67c
sabrina <sabrina130102@gmail.com>
2026-05-25 15:18:43 +0200

    removed white spaces. changed logolink router

--- src/component/Navbar/Navbar.jsx
+++ src/component/Navbar/Navbar.jsx
@@ -1,96 +1,96 @@
-import React, { useState } from "react"; 
+import React, { useState } from "react";
 import "./Navbar.css";
 import { Link } from "react-scroll";
-import logoLink from "../../assets/sab-logoName.png";
+import logoLink from "../../assets/brand/sab-logoName.png";
 
 export default function Navbar() {
     const [isOpen, setIsOpen] = useState(false);
 
     return (
         <nav id="nav">
-        <div className="nav-container">
-            <img className="logo-navbar" src={logoLink} alt="Logo" />
-            
-            <div className="menu"
-            onClick={() => {
-                setIsOpen(!isOpen);
-            }}
-            >
-                <span></span>
-                <span></span>
-                <span></span>
+            <div className="nav-container">
+                <img className="logo-navbar" src={logoLink} alt="Logo" />
+
+                <div
+                    className="menu"
+                    onClick={() => {
+                        setIsOpen(!isOpen);
+                    }}
+                >
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                </div>
+
+                <ul className={`nav-links ${isOpen ? "open" : ""}`}>
+                    <li>
+                        <Link
+                            className="nav-link"
+                            to="home"
+                            spy={true}
+                            smooth={true}
+                            offset={-70}
+                            duration={500}
+                            activeClass="active"
+                        >
+                            Home
+                        </Link>
+                    </li>
+                    <li>
+                        <Link
+                            className="nav-link"
+                            to="work"
+                            spy={true}
+                            smooth={true}
+                            offset={-70}
+                            duration={500}
+                            activeClass="active"
+                        >
+                            Project
+                        </Link>
+                    </li>
+
+                    <li>
+                        <Link
+                            className="nav-link"
+                            to="about"
+                            spy={true}
+                            smooth={true}
+                            offset={-70}
+                            duration={500}
+                            activeClass="active"
+                        >
+                            About
+                        </Link>
+                    </li>
+                    <li>
+                        <Link
+                            className="nav-link"
+                            to="skills"
+                            spy={true}
+                            smooth={true}
+                            offset={-70}
+                            duration={500}
+                            activeClass="active"
+                        >
+                            Skills
+                        </Link>
+                    </li>
+                    <li>
+                        <Link
+                            className="nav-link"
+                            to="contact"
+                            spy={true}
+                            smooth={true}
+                            offset={0}
+                            duration={500}
+                            activeClass="active"
+                        >
+                            Contact me
+                        </Link>
+                    </li>
+                </ul>
             </div>
-
-            <ul className={`nav-links ${isOpen ? "open" : ""}`}>    
-                <li>
-                    <Link
-                        className="nav-link"
-                        to="home"
-                        spy={true}
-                        smooth={true}
-                        offset={-70}
-                        duration={500}
-                        activeClass="active"
-                    >
-                        Home
-                    </Link>
-                </li>
-                 <li>
-                    <Link
-                        className="nav-link"
-                        to="work"
-                        spy={true}
-                        smooth={true}
-                        offset={-70}
-                        duration={500}
-                        activeClass="active"
-                    >
-                        Project
-                    </Link>
-                </li>
-
-                <li>
-                    <Link
-                        className="nav-link"
-                        to="about"
-                        spy={true}
-                        smooth={true}
-                        offset={-70}
-                        duration={500}
-                        activeClass="active"
-                    >
-                        About
-                    </Link>
-                </li>
-                <li>
-                    <Link
-                        className="nav-link"
-                        to="skills"
-                        spy={true}
-                        smooth={true}
-                        offset={-70}
-                        duration={500}
-                        activeClass="active"
-                    >
-                        Skills
-                    </Link>
-                </li>
-                <li>
-                    <Link
-                        className="nav-link"
-                        to="footer"
-                        spy={true}
-                        smooth={true}
-                        offset={0}
-                        duration={500}
-                        activeClass="active"
-                    >
-                        Contact me
-                    </Link>
-                </li>
-                
-            </ul>
-        </div>
-    </nav>
-    )
+        </nav>
+    );
 }
