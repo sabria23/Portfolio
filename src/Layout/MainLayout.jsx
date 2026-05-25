8d631aab851f77f7ceb32284c2c16a0929cf8aaf
sabrina <sabrina130102@gmail.com>
2026-05-25 15:20:40 +0200

    created mainLayout

---
+++ src/Layout/MainLayout.jsx
@@ -0,0 +1,22 @@
+import { Outlet } from "react-router-dom";
+
+import Navbar from "../component/Navbar/Navbar";
+import Footer from "../component/Footer/Footer";
+
+function MainLayout() {
+  return (
+    <>
+      <div className="page-container">
+        <Navbar />
+
+        <main className="content">
+          <Outlet />
+        </main>
+      </div>
+
+      <Footer />
+    </>
+  );
+}
+
+export default MainLayout;
